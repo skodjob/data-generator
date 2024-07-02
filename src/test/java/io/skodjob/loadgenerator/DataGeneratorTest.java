f2db13fecc8472f3a496253f00458d9b5050f79d
Jakub Stejskal <xstejs24@gmail.com>
2024-07-02 11:41:39 +0200

    Create parametrized test to check generated data
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

--- src/test/java/io/skodjob/loadgenerator/DataGeneratorTest.java
+++ src/test/java/io/skodjob/loadgenerator/DataGeneratorTest.java
@@ -8,8 +8,15 @@ import com.fasterxml.jackson.databind.JsonNode;
 import io.skodjob.loadgenerator.enums.ETemplateType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,18 +30,17 @@ class DataGeneratorTest {
         iotDataGenerator = new DataGenerator(ETemplateType.IOT_DEVICE);
     }
 
-    @Test
-    void testGenerateStringDataPayroll() {
-        String data = payrollDataGenerator.generateStringData();
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("testRepeatedParameters")
+    void testValidateGeneratedData(String testName, String regex, DataGenerator generator) {
+        Pattern dataPattern = Pattern.compile(regex);
+
+        String data = generator.generateStringData();
         assertNotNull(data, "Generated string data should not be null for PAYROLL_EMPLOYEE");
         assertFalse(data.contains("\"@"), "Generated string data should not contain placeholders for PAYROLL_EMPLOYEE");
-    }
 
-    @Test
-    void testGenerateStringDataIot() {
-        String data = iotDataGenerator.generateStringData();
-        assertNotNull(data, "Generated string data should not be null for IOT_DEVICE");
-        assertFalse(data.contains("\"@"), "Generated string data should not contain placeholders for IOT_DEVICE");
+        Matcher matcher = dataPattern.matcher(data);
+        assertTrue(matcher.matches(), "Generated data doesn't match the expected format!");
     }
 
     @Test
@@ -59,5 +65,39 @@ class DataGeneratorTest {
         }, "Expected generateStringData() to throw an exception, but it didn't");
 
         assertTrue(thrown.getMessage().contains("TemplateType cannot be null!"), "Exception message should contain 'TemplateType cannot be null!'");
+    }
+
+    public static Stream<Arguments> testRepeatedParameters() {
+        String peopleRegex = "\\{"
+            + "\"employee_id\":\"\\d+\","
+            + "\"first_name\":\"[A-Za-z]+\","
+            + "\"last_name\":\"[A-Za-z]+\","
+            + "\"age\":\\d+,"
+            + "\"ssn\":\"\\d+-\\d+-\\d+\","
+            + "\"hourly_rate\":\\d+\\.\\d+,"
+            + "\"gender\":\"(Male|Female)\","
+            + "\"email\":\"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\","
+            + "\"company\":\"[A-Za-z-& ]+\""
+            + "\\}";
+
+        String iotRegex = "\\{"
+            + "\"IPV4\":\"\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\","
+            + "\"MAC\":\"([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}\","
+            + "\"ID\":\"\\d+\","
+            + "\"TYPE\":\"(light|button|thermometer|plug|custom|gate)\","
+            + "\"LAST_UPDATE\":\"\\d+\","
+            + "\"LINK_QUALITY\":\\d{1,3},"
+            + "\"DATA\":\\{.*\\}"
+            + "\\}";
+
+
+        return IntStream.range(0, 10000)
+            .mapToObj(i -> Stream.of(
+                Arguments.of(ETemplateType.PAYROLL_EMPLOYEE.getTemplateName(), peopleRegex,
+                    new DataGenerator(ETemplateType.PAYROLL_EMPLOYEE)),
+                Arguments.of(ETemplateType.IOT_DEVICE.getTemplateName(), iotRegex,
+                    new DataGenerator(ETemplateType.IOT_DEVICE))
+            ))
+            .flatMap(stream -> stream);
     }
 }
