368680a58da893da5b96afb5d463c1a7cbd18c92
Jakub Stejskal <xstejs24@gmail.com>
2024-07-12 13:27:01 +0200

    Add scripts for releasing and update pom to allow pushing to sonatype (#12)
    ## Description
    
    This PR contains scripts and workflows that handles releasing to github
    and sonatype.
    
    ## Type of Change
    
    * New feature (non-breaking change which adds functionality)
    
    ## Checklist
    
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    
    ---------
    
    Signed-off-by: Jakub Stejskal <xstejs24@gmail.com>

---
+++ .github/scripts/push-to-nexus.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export GPG_TTY=$(tty)
+
+echo $GPG_SIGNING_KEY | base64 -d > signing.gpg
+gpg --batch --import signing.gpg
+
+GPG_EXECUTABLE=gpg mvn -DskipTests -s ./.github/scripts/settings.xml -P central-sonatype clean deploy
+
+rm -rf signing.gpg
+gpg --delete-keys
+gpg --delete-secret-keys
